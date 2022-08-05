47790adfc13bf7c3227c940ff7fb78c9ce78b1a9
hanseok kim <hasuikhs@gmail.com>
2022-08-05 23:51:51 +0900

    220805|Update selenium

--- server/crawler.ts
+++ server/crawler.ts
@@ -6,8 +6,10 @@ import 'chromedriver';
 async function example() {
 
   const options = new chrome.Options().addArguments(
-    '--headless',
-    '--no--sandbox'
+    '--headless', // browser 없이 실행
+    '--no--sandbox', // GUI 없는 환경 추가 옵션
+    '--disable-gpu',  // GUI 없는 환경 추가 옵션
+    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
   );
 
   const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
@@ -15,7 +17,10 @@ async function example() {
   try {
     await driver.get('http://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 2000);
+
+    let userAgent = await driver.executeScript("return navigator.userAgent");
+    console.log(userAgent)
   } finally {
     await driver.quit();
   }
