536c6fbcaf9c0771f8b33dcd0399326b5d37b928
hanseok kim <hasuikhs@gmail.com>
2022-08-07 23:00:24 +0900

    220807|Update package.json

--- server/crawler.ts
+++ server/crawler.ts
@@ -6,10 +6,13 @@ import 'chromedriver';
 async function example() {
 
   const options = new chrome.Options().addArguments(
-    '--headless', // browser 없이 실행
-    '--no--sandbox', // GUI 없는 환경 추가 옵션
-    '--disable-gpu',  // GUI 없는 환경 추가 옵션
-    'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
+    // '--headless', // browser 없이 실행
+    // '--no--sandbox', // GUI 없는 환경 추가 옵션
+    // '--disable-gpu',  // GUI 없는 환경 추가 옵션
+    '--incognito',  // 시크릿 모드
+    '--disable-infobars',
+    'lang=ko_KR',
+    // 'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
   );
 
   const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
